02609a7366b4bdc49e26d5a65b33193bc98764eb
Allduiin <vovapro01@gmail.com>
2021-07-22 10:27:22 +0300

    Added checkstyle plugin and change style errors

--- src/main/java/test/task/catalogs/model/Catalog.java
+++ src/main/java/test/task/catalogs/model/Catalog.java
@@ -1,9 +1,13 @@
 package test.task.catalogs.model;
 
-import lombok.Data;
-
-import javax.persistence.*;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.Data;
 
 @Entity
 @Data
