8b9a92cac869fa21c59e551f894e37e3d228cb32
Allduiin <vovapro01@gmail.com>
2021-07-23 15:11:05 +0300

    Reworked Count and getCount method

--- src/main/java/test/task/catalogs/model/Catalog.java
+++ src/main/java/test/task/catalogs/model/Catalog.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 import lombok.Data;
 
 @Entity
@@ -20,8 +21,16 @@ public class Catalog {
     private Catalog fatherCatalog;
     @OneToMany
     private List<Catalog> childCatalogs;
+    @Transient
+    private Long count;
 
     public Long getFatherId() {
         return fatherCatalog.getId();
+    }
+
+    public Long getCount() {
+        return childCatalogs.size() + childCatalogs.stream()
+                .mapToLong(Catalog::getCount)
+                .sum();
     }
 }
